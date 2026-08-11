a00152026f3a148bcdb78361625bbf4e289502b9
Erica Madebeykin <erica.madebeykina@gmail.com>
2026-08-11 16:26:58 +0400

    added Circle and Rectangle

--- HWU_practice/src/Circle.java
+++ HWU_practice/src/Circle.java
@@ -1,2 +1,40 @@
-public class Circle {
+import java.lang.Math;
+
+public class Circle extends GeometricObject{
+
+    private double radius;
+
+    /** default constructor*/
+    public Circle(){
+        super();
+    }
+
+    /** Constructor with radius*/
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    /** This getter method returns radius*/
+    public double getRadius(){
+        return radius;
+    }
+
+    /** This setter method sets the radius of the circle to the specified value*/
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    public double getDiameter(){
+        return 2 * radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius * radius *Math.PI;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return radius * 2 * Math.PI;
+    }
 }
--- HWU_practice/src/Rectangle.java
+++ HWU_practice/src/Rectangle.java
@@ -1,2 +1,45 @@
-public class Rectangle {
+public class Rectangle extends GeometricObject{
+    private double width;
+    private double height;
+
+    /** This is the default constructor with super*/
+    public Rectangle(){
+        super();
+    }
+    /** Now this is our constructor with parameters*/
+    public Rectangle (double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    /** This getter method returns width */
+    public double getWidth(){
+        return width;
+    }
+
+    /** This getter method returns height */
+    public double getHeight(){
+        return height;
+    }
+
+    /** Let's do two setter methods for width and height */
+
+    public void sedWidth(double width){
+        this.width= width;
+    }
+
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    /** Now let's completely define the override methods from the parent class */
+    @Override
+    public double getArea() {
+        return width*height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*width*height;
+    }
 }
