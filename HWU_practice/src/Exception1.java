6dfb5cf73cefb2cce9c57ea3155c257b5a2be1cc
Erica Madebeykin <erica.madebeykina@gmail.com>
2026-08-11 19:48:24 +0400

    exceptions

--- HWU_practice/src/Exception1.java
+++ HWU_practice/src/Exception1.java
@@ -7,6 +7,8 @@ public class Exception1 {
         UncheckedExceptionExample();
         ArithmeticExceptionExample();
         MultipleTryCatchExample();
+        NestedTryCatchExample();
+        MyOtherThrowExample();
     }
 
     public static void CheckedExceptionExample() {
@@ -44,18 +46,60 @@ public class Exception1 {
         }
     }
 
-    public static void MultipleTryCatchExample(){
-           try{
-               int[] numbers = {1, 2,3,4};
-               System.out.println(numbers[6]);
-           } catch(ArithmeticException| ArrayIndexOutOfBoundsException e){
-               if(e instanceof ArithmeticException){
-                   System.out.println("Arithmetic error: " + e.getMessage());
-               }else{
-                   System.out.println("Error: " + e.getMessage());
-               }
+    public static void MultipleTryCatchExample() {
+        try {
+            int[] numbers = {1, 2, 3, 4};
+            System.out.println(numbers[6]);
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+            String errorMessage = e.getMessage();
+            System.out.println("=========================");
+            System.out.println("Multiple Catch Block error: "+ errorMessage);
 
+            if (e instanceof ArithmeticException) {
+                System.out.println("Arithmetic error: " + e.getMessage());
+            } else {
+                System.out.println("Error: " + errorMessage);
+            }
         }
     }
+    public static void NestedTryCatchExample() {
+        try {
+            int[] numbers = { 1, 2, 3, 4 };
+            System.out.println(numbers[6]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("=========================");
+            System.out.println("Array index out of bounds. Error: " + e.getMessage());
+            try {
+                int result = 10 / 0;
+            } catch (ArithmeticException ex) {
+                System.out.println("=========================");
+                System.out.println("Arithmetic error: " + e.getMessage());
+            }
+        }finally{
+            System.out.println("=========================");
+        }
+    }
+
+    /** This is complicated, throws should be handled either in main method (problematic)
+     * or should be better just use try-catch block
+    public static void MyThrowExample() throws Exception {
+        throw new Exception("This is a thrown exception");
+    } */
+        
+    public static void MyOtherThrowExample() {
+    try {
+        throw new Exception("This is a thrown exception");
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+    }
+}
+ 
+
+
+
+
+
+
+
 
 }
